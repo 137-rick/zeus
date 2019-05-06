befb01f906d08e60f9be4012d19c66b804d0d1f7
xuchanglong <xuchanglong@100tal.com>
2019-05-06 18:37:58 +0800

    add thread process request

--- zeus.cpp
+++ zeus.cpp
@@ -1,6 +1,6 @@
 #include "zeus.hpp"
 #include "zssocket.h"
-
+#include <csignal>
 
 zeus::zeus(std::string ip, int port) {
 
@@ -18,7 +18,19 @@ void zeus::on_receive_thread(int fd) {
 
 }
 
+void handle_pipe(int sig) {
+    //不做任何处理即可
+}
+
 void zeus::server_start() {
+
+    //signal
+    struct sigaction action;
+    action.sa_handler = handle_pipe;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    sigaction(SIGPIPE, &action, NULL);
+
     //running flag
     this->running = true;
 
@@ -40,11 +52,11 @@ void zeus::accept_socket_thread() {
         int connfd = zssocket::accept_conn(this->accept_info.listenfd_int, &(this->accept_info.sockeaddr_st));
         if (connfd > 0) {
 
+            //send with fd
             int thread_id = connfd % 16;
 
             //add request event monitor with epoll
-            zsepoll::add_epoll_event(this->event_thread_list[thread_id].event_epoll_fd
-                    , connfd, EPOLLIN);
+            zsepoll::add_epoll_event(this->event_thread_list[thread_id].event_epoll_fd, connfd, EPOLLIN);
         }
     }
 
@@ -56,8 +68,7 @@ void zeus::event_wait_thread(int thread_id) {
 
     while (this->running) {
 
-        int ret = epoll_wait(this->event_thread_list[thread_id].event_epoll_fd
-                , eventlist_st, SOCKET_MAX_EVENTS, 3000);
+        int ret = epoll_wait(this->event_thread_list[thread_id].event_epoll_fd, eventlist_st, SOCKET_MAX_EVENTS, 3000);
         if (ret < 0) {
             printf("epoll error %d,%s\n", errno, strerror(errno));
             break;
@@ -67,15 +78,38 @@ void zeus::event_wait_thread(int thread_id) {
         }
 
         for (int i = 0; i < ret; i++) {
+
+            //close event
+
+            if ((eventlist_st[i].events & EPOLLIN) &&
+                (eventlist_st[i].events & EPOLLRDHUP)) {
+                //http close by client
+                printf("epoll event close\n");
+                close(eventlist_st[i].data.fd);
+                continue;
+            }
+
+            //todo:request
+            if (eventlist_st[i].events & EPOLLIN) {
+                //read 0 when closed
+                //http process
+                zssocket::recv_data(eventlist_st[i].data.fd);
+                std::string data = "HTTP/1.1 200 OK\r\nContent-Length: 3\r\nConnection: keep-alive\r\n\r\n123";
+                zssocket::send_data(data, eventlist_st[i].data.fd);
+            }
+
+            //todo:can send
+            if (eventlist_st[i].events & EPOLLOUT) {
+
+            }
+
             if (eventlist_st[i].events & EPOLLERR ||
-                eventlist_st[i].events & EPOLLHUP ||
-                !(eventlist_st[i].events & EPOLLIN)) {
+                eventlist_st[i].events & EPOLLHUP) {
                 printf("epoll event error event:%d fd:%d\n", eventlist_st[i].events, eventlist_st[i].data.fd);
                 close(eventlist_st[i].data.fd);
                 continue;
             }
 
-            //http process
 
         }
     }
